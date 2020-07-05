4fa6afccb19aef24e7ff08598840e9d4e5843e71
couldBefree <zoriaalexandr@gmail.com>
2020-07-05 14:07:48 +0300

    polaris

---
+++ pages/_app.js
@@ -0,0 +1,36 @@
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react'
+import '@shopify/polaris/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
+import Cookies from 'js-cookie';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    fetchOptions: {
+        credentials: 'include'
+    }
+});
+
+
+class MyApp extends App {
+    render() {
+        const { Component, pageProps } = this.props;
+
+        return (
+            <React.Fragment>
+                <Head>
+                    <title>Sample App</title>
+                    <meta charSet="utf-8" />
+                </Head>
+                <AppProvider i18n={translations}>
+                    <Component {...pageProps} />
+                </AppProvider>
+            </React.Fragment>
+        );
+    }
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-const Index = () => (
-    <div>
-        <p>Shopify app</p>
-    </div>
-)
+import { EmptyState, Layout, Page } from '@shopfify/polaris'
+
+function Index() {
+    return (
+        <Page>
+            <Layout>
+                <EmptyState>
+                    <p>Select Products</p>
+                </EmptyState>
+            </Layout>
+        </Page>
+    )
+}
 
 export default Index;
